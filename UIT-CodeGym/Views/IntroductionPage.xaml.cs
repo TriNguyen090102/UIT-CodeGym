552b9fdf5cd821bce39664271afb4a6be140274a
Minhgaa <bminh.cv@gmail.com>
2023-12-07 12:55:11 +0700

    fixbackground

--- UIT-CodeGym/Views/IntroductionPage.xaml.cs
+++ UIT-CodeGym/Views/IntroductionPage.xaml.cs
@@ -11,11 +11,4 @@ public partial class IntroductionPage : ContentPage
 		BindingContext = vm;
 	}
 
-	
-
-	protected override void OnSizeAllocated (double pageWidth, double pageHeight) {
-    base.OnSizeAllocated(pageWidth, pageHeight);
-    const double aspectRatio = 1600 / 1441.0; // Aspect ratio of the original image
-    backgroundImage.WidthRequest = Math.Max(pageHeight * aspectRatio, pageWidth);
-}
 }
