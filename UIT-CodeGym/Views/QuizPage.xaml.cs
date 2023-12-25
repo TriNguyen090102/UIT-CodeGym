7650ae6d47bfaddc56c0d27a923bc82426298f29
TriNguyen090102 <22521523@gm.uit.edu.vn>
2023-12-25 14:08:23 +0700

    check correct ans and change background

--- UIT-CodeGym/Views/QuizPage.xaml.cs
+++ UIT-CodeGym/Views/QuizPage.xaml.cs
@@ -8,6 +8,9 @@ public partial class QuizPage : ContentPage
     Button previousButton;
     List<char> answer = Enumerable.Repeat('0', 10).ToList();
     QuestionsService service = new QuestionsService();
+    List<Button> selectedButton = new List<Button>();
+    List<Button> correctButton = new List<Button>();
+
     List<QuestionModel> questions = new List<QuestionModel>();
 
     public QuizPage(QuizPageVM vm)
@@ -15,7 +18,11 @@ public partial class QuizPage : ContentPage
         InitializeComponent();
         questions = vm.PopulateOperatorQuestions();
         BindingContext = vm;
-
+        for (int i = 0; i < 10; i++)
+        {
+            selectedButton.Add(null);  // Add null as the initial value
+            correctButton.Add(null);
+        }
     }
     void Button_ClickedA(System.Object sender, System.EventArgs e)
     {
@@ -65,6 +72,22 @@ public partial class QuizPage : ContentPage
             if (childView is Button button)
             {
                 button.BackgroundColor = Color.FromHex("#FFFFFF");
+                if (button.Text == questions[0].a && questions[0].correct_answer == "a")
+                {
+                    correctButton[0] = button;
+                }
+                if (button.Text == questions[0].b && questions[0].correct_answer == "b")
+                {
+                    correctButton[0] = button;
+                }
+                if (button.Text == questions[0].c && questions[0].correct_answer == "c")
+                {
+                    correctButton[0] = button;
+                }
+                if (button.Text == questions[0].d && questions[0].correct_answer == "d")
+                {
+                    correctButton[0] = button;
+                }
             }
         }
         Button clickedButton = (Button)sender;
@@ -76,8 +99,25 @@ public partial class QuizPage : ContentPage
         {
             clickedButton.BackgroundColor = Color.FromHex("#E1EEBC");
         }
-        
 
+
+        if (clickedButton.Text == questions[0].a)
+        {
+            answer[0] = 'a';
+        }
+        else if (clickedButton.Text == questions[0].b)
+        {
+            answer[0] = 'b';
+        }
+        else if (clickedButton.Text == questions[0].c)
+        {
+            answer[0] = 'c';
+        }
+        else if (clickedButton.Text == questions[0].d)
+        {
+            answer[0] = 'd';
+        }
+        selectedButton[0] = clickedButton;
     }
 
     void Question2_Handle_Clicked(object sender, System.EventArgs e)
@@ -87,6 +127,21 @@ public partial class QuizPage : ContentPage
             if (childView is Button button)
             {
                 button.BackgroundColor = Color.FromHex("#FFFFFF");
+                if(button.Text == questions[1].a && questions[1].correct_answer == "a") {
+                    correctButton[1] = button;
+                }
+                if (button.Text == questions[1].b && questions[1].correct_answer == "b")
+                {
+                    correctButton[1] = button;
+                }
+                if (button.Text == questions[1].c && questions[1].correct_answer == "c")
+                {
+                    correctButton[1] = button;
+                }
+                if (button.Text == questions[1].d && questions[1].correct_answer == "d")
+                {
+                    correctButton[1] = button;
+                }
             }
         }
         Button clickedButton = (Button)sender;
@@ -98,7 +153,38 @@ public partial class QuizPage : ContentPage
         {
             clickedButton.BackgroundColor = Color.FromHex("#E1EEBC");
         }
-        
 
+        if (clickedButton.Text == questions[1].a)
+        {
+            answer[1] = 'a';
+          
+        }
+        else if(clickedButton.Text == questions[1].b)
+        {
+            answer[1] = 'b';
+           
+        }
+        else if (clickedButton.Text == questions[1].c)
+        {
+            answer[1] = 'c';
+           
+        }
+        else if (clickedButton.Text == questions[1].d)
+        {
+            answer[1] = 'd';
+        }
+        selectedButton[1] = clickedButton;
+    }
+
+    void OnFinish(object sender, System.EventArgs e)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            if(answer[i].ToString() != questions[i].correct_answer)
+            {
+                selectedButton[i].BackgroundColor = Color.FromHex("#db3939");
+            }
+            correctButton[i].BackgroundColor = Color.FromHex("#E1EEBC");
+        }
     }
 }
